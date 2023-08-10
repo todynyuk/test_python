9f46a74653b444f9f921c8adbddab7fd3f91c978
todynyuk <todynyuk@solvd.com>
2023-08-10 14:32:32 +0300

    update test file

--- test_rozetka.py
+++ test_rozetka.py
@@ -1,14 +1,15 @@
 import pytest
-import time, os, re, shutil, sys
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver import ActionChains
+# import time, os, re, shutil, sys
+import time
+# from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver import ActionChains
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from unittest import TestCase
 
