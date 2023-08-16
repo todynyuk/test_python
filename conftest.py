c3fc5c3c297a2c266ec175996384e3df41e18bca
todynyuk <todynyuk@solvd.com>
2023-08-16 19:00:19 +0300

    delete conftest.py and modify .env file

--- conftest.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-
-options = webdriver.ChromeOptions()
-options.platform_name = "linux"
-options.browser_version = "latest"
-options.set_capability("enableVideo", "true")
